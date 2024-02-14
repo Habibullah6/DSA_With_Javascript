530577b371f015c8b924943333abfaadc9e75c82
habibullah6 <nomanhabibullah65@gmail.com>
2024-02-15 00:33:13 +0600

    done

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,38 @@
-let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
-let n = myArray.length;
+// let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+// let n = myArray.length;
 
-for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-        if (myArray[j] > myArray[j + 1]) {
-            let temp = myArray[j];
-            myArray[j] = myArray[j + 1];
-            myArray[j + 1] = temp;
+// for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//         if (myArray[j] > myArray[j + 1]) {
+//             let temp = myArray[j];
+//             myArray[j] = myArray[j + 1];
+//             myArray[j + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log("Sorted array:", myArray);
+
+
+function bubbleSort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        
+        for (let j = 0; j < arr.length - i - 1; j++) {
+           
+            if(arr[j] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+            
         }
+        
     }
+
+    return arr;
 }
 
-console.log("Sorted array:", myArray);
+const result = [5, 1, 3, 4, 2];
+const output = bubbleSort(result);
+console.log(output)
