a1e69189e09cb1739693c73b64e4d5661bbda7a5
habibullah6 <nomanhabibullah65@gmail.com>
2024-02-15 00:26:34 +0600

    done

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+let n = myArray.length;
+
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        if (myArray[j] > myArray[j + 1]) {
+            let temp = myArray[j];
+            myArray[j] = myArray[j + 1];
+            myArray[j + 1] = temp;
+        }
+    }
+}
+
+console.log("Sorted array:", myArray);
